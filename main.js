29436f8f22780011d3739f1fbc727b7427e9a881
Lil.Dre_ <109699805+LilDre7@users.noreply.github.com>
2022-11-30 11:26:22 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// Mi nombre
+let nombre ="Alvaro Aburto Ocampo";
+// Mi edad
+let edad = 19 ;
+document.write (nombre  ,"<br>" , "<br>", edad , "<br>" , "<br>" );
+
+// ¿Eres desarrollador?
+const a = "Soy developer"; 
+const b = "No soy developer"
+
+document.write( a != b , "<br>");
+
+// Fecha de nacimiento
+let miCumpleaños = new Date(" Jan 3 2003")
+document.write("<br>",miCumpleaños)
+
+// Tu libro favorito / Objeto con propiedades : Titular , auto , fecha , url
+
+let libro = {
+    Titular: "Mas alla del invierno",
+    Autor: "Isabell Allende",
+    Fecha: " 1 / 2 / 2004",
+    url: "https://www.pexels.com/search/computer/"
+} 
+console.log(libro)
